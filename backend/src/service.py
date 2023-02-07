34fc1b64461e0a04b9555b9234e578688f4e356d
pavelprokhorenko <prohorenkopavel74@gmail.com>
2023-02-07 19:37:05 +0400

    add env settings to service

--- backend/src/service.py
+++ backend/src/service.py
@@ -1,58 +1,57 @@
-from os import system
-
 from fastapi.applications import FastAPI
 from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
+from src.config.settings import BackendSettings, settings
 from uvicorn import run
-
-from settings import Settings, settings
 
 
 class Service:
-    def __init__(self, service_settings: Settings) -> None:
+    __slots__ = ("_settings", "_asgi_app")
+
+    def __init__(self, backend_settings: BackendSettings) -> None:
         """
         Initiation ASGI app.
         """
-        self.settings = service_settings
+        self._settings = backend_settings
 
         middleware = [
             Middleware(
                 CORSMiddleware,
-                allow_origins=settings.service.origins,
+                allow_origins=self._settings.ALLOWED_ORIGINS,
                 allow_credentials=True,
-                allow_methods=["POST"],
-                allow_headers=["*"],
+                allow_methods=self._settings.ALLOWED_METHODS,
+                allow_headers=self._settings.ALLOWED_HEADERS,
             ),
         ]
 
-        self.main_app = FastAPI(middleware=middleware)
+        self._asgi_app = FastAPI(middleware=middleware)
 
     def get_app(self) -> FastAPI:
         """
         Get ASGI app.
         """
-        return self.main_app
+        return self._asgi_app
 
     def serve(self, path_to_app: str) -> None:
         """
         Start service.
         """
-        if self.settings.service.debug:
+        if self._settings.DEBUG:
             run(
                 path_to_app,
-                host=self.settings.service.host,
-                port=self.settings.service.port,
+                host=self._settings.SERVER_HOST,
+                port=self._settings.SERVER_PORT,
                 reload=True,
                 use_colors=True,
                 debug=True,
             )
         else:
-            system(
-                f"uvicorn {path_to_app} --host"
-                f" {self.settings.service.host} --port"
-                f" {self.settings.service.port} --workers"
-                f" {self.settings.service.workers}"
+            run(
+                path_to_app,
+                host=self._settings.SERVER_HOST,
+                port=self._settings.SERVER_PORT,
+                workers=self._settings.SERVER_WORKERS,
             )
 
 
-service = Service(service_settings=settings)
+service = Service(backend_settings=settings)
