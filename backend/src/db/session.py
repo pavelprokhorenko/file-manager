97174b39d8afc5701cd8209becd4907073547cb2
pavelprokhorenko <prohorenkopavel74@gmail.com>
2023-02-08 14:16:36 +0400

    add postgres connection manager

--- backend/src/db/session.py
+++ backend/src/db/session.py
@@ -1,22 +1,39 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
-from src.config.settings import settings
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from src.config.settings import BackendSettings, settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://{}:{}@{}:{}/{}".format(
-    settings.POSTGRES_USERNAME,
-    settings.POSTGRES_PASSWORD,
-    settings.POSTGRES_HOST,
-    settings.POSTGRES_PORT,
-    settings.POSTGRES_NAME,
-)
 
-engine = create_async_engine(
-    url=SQLALCHEMY_DATABASE_URL, echo=settings.IS_POSTGRES_ECHO_LOG, pool_pre_ping=True
-)
-Session = sessionmaker(
-    engine,
-    class_=AsyncSession,
-    autoflush=False,
-    autocommit=False,
-    expire_on_commit=settings.IS_POSTGRES_SESSION_EXPIRE_ON_COMMIT,
-)
+class AsyncPostgres:
+    """
+    Async PostgreSQL connection manager.
+    """
+
+    def __init__(self, backend_settings: BackendSettings) -> None:
+        self._postgres_url = (
+            "postgresql+asyncpg://{username}:{password}@{host}:{port}/{db_name}".format(
+                username=backend_settings.POSTGRES_USERNAME,
+                password=backend_settings.POSTGRES_PASSWORD,
+                host=backend_settings.POSTGRES_HOST,
+                port=backend_settings.POSTGRES_PORT,
+                db_name=backend_settings.POSTGRES_NAME,
+            )
+        )
+
+        self._async_engine = create_async_engine(
+            url=self._postgres_url,
+            echo=backend_settings.IS_POSTGRES_ECHO_LOG,
+            pool_pre_ping=True,
+        )
+        self._async_session = async_sessionmaker(
+            bind=self._async_engine,
+            class_=AsyncSession,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=backend_settings.IS_POSTGRES_SESSION_EXPIRE_ON_COMMIT,
+        )
+
+    @property
+    def async_session(self) -> async_sessionmaker[AsyncSession]:
+        return self._async_session
+
+
+async_postgres = AsyncPostgres(backend_settings=settings)
