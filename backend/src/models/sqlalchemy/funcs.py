8f41fffef014ccdafe58accfeaf8d44938c651b4
pavelprokhorenko <prohorenkopavel74@gmail.com>
2023-02-08 16:24:56 +0400

    add utcnow impl for postgres

---
+++ backend/src/models/sqlalchemy/funcs.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.sql import expression
+from sqlalchemy.types import DateTime
+
+
+class utcnow(expression.FunctionElement):  # noqa
+    """
+    Implementation of current datetime in UTC timezone for different database dialects.
+    """
+
+    type = DateTime()
+    inherit_cache = True
+
+
+@compiles(utcnow, "postgresql")
+def postgres_utcnow(*args, **kwargs) -> str:
+    """
+    PostgreSQL impl of current datetime in UTC timezone.
+    """
+    return "TIMEZONE('utc', CURRENT_TIMESTAMP)"
