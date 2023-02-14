23fbd189e53fa59b241481726455c1ec1a17d4f3
pavelprokhorenko <prohorenkopavel74@gmail.com>
2023-02-14 11:05:30 +0400

    remove id from Entity fields

--- backend/src/domain/common/entity.py
+++ backend/src/domain/common/entity.py
@@ -8,17 +8,15 @@ class Entity(BaseModel):
     Generic entity schema.
     """
 
-    id: Any
-
     @property
     def row_id(self) -> Any:
         """
         To avoid using a reserved Python name "id" use row_id instead.
         """
-        if hasattr(self, "id") and self.id is not None:
-            return self.id
+        if hasattr(self, "id"):
+            return getattr(self, "id")
 
-        raise AttributeError(f'Entity {self.__repr_name__()} has no attribute "id"')
+        raise AttributeError(f'Entity "{self.__repr_name__()}" has no attribute "id"')
 
     class Config:
         orm_mode = True
