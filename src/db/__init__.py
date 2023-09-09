8b3527f30cfe5c0ef2655ba0047be48e0e09742a
pavelprokhorenko <prohorenkopavel74@gmail.com>
2023-09-09 22:28:47 +0700

    ref: separate file_system_node to file and folder tables

--- src/db/__init__.py
+++
@@ -1,7 +0,0 @@
-from src.db.base import Base
-from src.db.session import async_postgres
-
-__all__ = (
-    "Base",
-    "async_postgres",
-)
--- src/db/base.py
+++ src/db/base.py
@@ -1,6 +1,10 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
 from src.db.base_class import Base
-from src.modules.file_system.models import FileSystemNode
+from src.modules.file_system.models import File, Folder
 
-__all__ = ("Base", "FileSystemNode")
+__all__ = (
+    "Base",
+    "File",
+    "Folder",
+)
